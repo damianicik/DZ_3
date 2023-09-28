69fec51489a4d9a530452c9729eeb1c42a436bf6
damianicik <volknatalia1986@yandex.ru>
2023-09-28 22:44:25 +0300

    Seminar_3_DZ_3

---
+++ DZ_3_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, 
+и возвращает расстояние между ними в 3D пространстве.*/
+
+double DistanceBetweenPoints(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+double DistanceBetweenPoints = Math.Round (Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1, 2)+Math.Pow(z2-z1, 2)), 3);
+return DistanceBetweenPoints;
+}
+
+Console.WriteLine ("Введите x1");
+double x1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите y1");
+double y1 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine ("Введите x2");
+double x2 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите y2");
+double y2 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine ("Введите z1");
+double z1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите z2");
+double z2 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine ("Расстояние равно " + DistanceBetweenPoints(x1, y1, z1, x2, y2, z2));
---
+++ DZ_3_task2/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов 
+чисел от 1 до N (включительно) каждое на новой строке.*/
+
+Console.WriteLine("введите число: ");
+int ShowCube = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=ShowCube; i++)// i++ увеличение индекса на 1
+{
+    System.Console.Write($"{Math.Pow(i,3)} ");
+    Console.WriteLine();
+}
+// System.Console.WriteLine();// чтоб был пробел между числами в выводе
