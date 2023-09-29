eec4ed1a05642585d1b187ea7f68b9aa7e39b192
damianicik <115208277+damianicik@users.noreply.github.com>
2023-09-29 07:16:09 +0300

    Update Program.cs

--- DZ_3_task3/Program.cs
+++ DZ_3_task3/Program.cs
@@ -3,12 +3,12 @@ string number = Console.ReadLine();
 
 void CheckingNumber(string number){
   if (number[0]==number[4] && number[1]==number[3]){
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
+    Console.WriteLine($"{number} - True");
   }
-  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+  else Console.WriteLine($"{number} - False");
 }
 
 if (number.Length == 5){
   CheckingNumber(number);
 }
-else Console.WriteLine($"Введи правильное число");
+else Console.WriteLine($"Ваше число не палиндром");
